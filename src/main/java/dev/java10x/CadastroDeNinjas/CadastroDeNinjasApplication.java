d4aee09762b611df70fee53583e42c3091c19708
Simone Frez <simonefrez25@gmail.com>
2026-09-01 15:14:30 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -3,6 +3,8 @@ package dev.java10x.CadastroDeNinjas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// marca a classe principal de um app spring boot
+// mvn spring-boot:run comando para iniciar o app spring boot
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
 
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// RestController define a classe como um controlador REST
 @RestController
 @RequestMapping
 public class NinjaController {
